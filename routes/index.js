8a778e91895fc11e212c4ed7cc0d3ef2e3133e86
Javier.Homs <javier.homs@gmail.com>
2015-06-12 12:21:57 +0200

    Anyadir preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,10 @@ router.param('quizId', quizController.load);
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+router.get('/quizes/new',quizController.new);
 router.get('/author', quizController.author);
+
+router.post('/quizes/create', quizController.create);
+
 
 module.exports = router;
